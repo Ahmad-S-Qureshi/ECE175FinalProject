be218689328ad8f36af67ba16662c0e8e7d85d1c
Ahmad-S-Qureshi <113152659+Ahmad-S-Qureshi@users.noreply.github.com>
2023-04-12 10:48:34 -0700

    Prep

---
+++ Final-Project/linkedListFunctions.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+//typedef node
+
+void createLinkedListHead() {
+
+}
+
+void createLinkedListNodeAtEnd(head headptr, int length) {
+
+}
+
+void shuffle(head headPtr, int length) {
+
+}
+
+void swap(node node1, node node2) {
+
+}
