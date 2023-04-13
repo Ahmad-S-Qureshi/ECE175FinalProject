f61430794dcf24d22368ae64958ea82f92ada502
Ahmad-S-Qureshi <113152659+Ahmad-S-Qureshi@users.noreply.github.com>
2023-04-12 17:38:39 -0700

    work on functions

--- Final-Project/linkedListFunctions.h
+++ Final-Project/linkedListFunctions.h
@@ -13,25 +13,26 @@ void createLinkedListHead() {
     return temp;
 }
 
-void createLinkedListTail(Node headptr, Node finalptr) {
+void createLinkedListAtEnd(Node *headptr, Node *finalptr) {
     Node temp;
     temp->prevPtr = finalptr;
     temp->nextPtr = NULL;
     return temp;
 }
 
-void nodeInsertAfter(Node* thisNode, Node* newNode) {
-   Node* tmpNext = NULL;
-   tmpNext = thisNode->nextNodePtr; // Remember next
-   thisNode->nextNodePtr = newNode; // this -- new -- ?
-   newNode->nextNodePtr = tmpNext;  // this -- new -- next
+void shuffle(Node *headPtr) {
+    
 }
 
-void shuffle(Node headPtr) {
-
+int getLinkedListLength(Node *headPtr) {
+    int returnVal = 0;
+    while(headPtr->nextPtr != NULL) {
+        returnVal++;
+    }
+    return returnVal;
 }
 
-void swapNodes(Node node1, Node node2) {
+void swapNodes(Node *node1, Node *node2) {
     prevPtr1 = node1->prevPtr;
     prevPtr2 = node2->prevPtr
     temp = prevPtr1;
