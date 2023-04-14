f4ecf541ecfbc9dd8dfc6435fb4e6f04a2ec7483
Ahmad-S-Qureshi <113152659+Ahmad-S-Qureshi@users.noreply.github.com>
2023-04-14 08:14:00 -0700

    Added Legibility

--- ECE175FinalProject-main/Final-Project-still-broken/main.c
+++ ECE175FinalProject-main/Final-Project-still-broken/main.c
@@ -9,7 +9,7 @@
 
 int main() {
     //Vector List of LinkedListData's to be Implemented
-    
+
     LinkedListData discardData;
     initializeData(discardData);
     FILE* fp = fopen("listOfCards.txt", "r");
@@ -18,17 +18,20 @@ int main() {
     discardData.head = &discardHeadNode;
     discardData.tail = initializeLinkedListFromFile(discardData, fp);
     strcpy((discardData.tail)->data.string, "This is the tail of the discard pile");
+
     printf("Printing LinkedList Forward\n");
     printList(discardData);
+
     printf("\nNow backwards:\n");
     printListBackwards(discardData);
 
     printf("\n\n\n\n");
+
     printNode(getElementFromIndex(&discardData, 0));
     printNode(getElementFromIndex(&discardData, getLinkedListLength(&discardData)-1));
-    printf("%d\n", getLinkedListLength(&discardData));
 
     printf("%d\n", getLinkedListLength(&discardData));
+    
     LinkedListData drawData;
     initializeData(drawData);
     Node drawHeadNode;
